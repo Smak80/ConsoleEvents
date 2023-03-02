09b5f6aa3620c9271e610d2f8484e3792c0a8c6f
Sergey Makletsov <smaklets@kpfu.ru>
2023-03-02 10:53:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleEvents/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using ConsoleEvents;
+
+var ps = new ParSummator(2000000000);
+var sw = new Stopwatch();
+void ShowResult()
+{
+    sw.Stop();
+    Console.WriteLine("{0} за {1} мс.", ps.Result, sw.ElapsedMilliseconds);
+}
+
+var ss = new SeqSummator(2000000000);
+
+sw.Start();
+ss.Sum();
+sw.Stop();
+Console.WriteLine("{0} за {1} мс.", ss.Result, sw.ElapsedMilliseconds);
+
+ps.Finish += ShowResult;
+sw.Restart();
+ps.Sum();
